f1d59d1d0d65b883726ab30daf0c84f27d34eb20
Ineza-Bernice <122214795+Ineza-Bernice@users.noreply.github.com>
2025-10-08 14:41:18 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,5 +22,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
